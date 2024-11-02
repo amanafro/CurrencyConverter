5e6fc47e20c16ff76564c68e9b93ad796ca75b3b
amanafro <diazermando193@gmail.com>
2024-11-02 23:20:04 +0100

    updated the code now i just need to add the converted amount and work on the api

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -12,8 +13,6 @@ import (
 	"github.com/charmbracelet/huh"
 )
 
-// WARNING: this code is shit
-
 type ExchangeRates struct {
 	Rates map[string]float64 `json:"rates"`
 }
@@ -21,7 +20,8 @@ type ExchangeRates struct {
 var (
 	BaseCurrency     string
 	CovertedCurrency string
-	Amount           string
+	Amount           float64
+	AmountStr        string
 	CovertedAmount   float64
 	confirm          bool
 )
@@ -34,10 +34,8 @@ func main() {
 		log.Fatalln("API key not found in environment variables. Please set the API_KEY environment variable.")
 	}
 
-	// FIX: the conversion vaule is not showing and i'm getting NaN as a response
-
 	fmt.Println("API Key:", appID)
-	url := fmt.Sprint("https://openexchangerates.org/api/latest.json?app_id=%s", appID)
+	url := fmt.Sprintf("https://openexchangerates.org/api/latest.json?app_id=%s", appID)
 
 	// Make the HTTP request
 	response, err := http.Get(url)
@@ -61,18 +59,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// NOTE: i had to parse the string into float64, because for some reason the value() function only accepts string and i was lazy to not ckeck or try to find a solution
-
-	amountFloat, err := strconv.ParseFloat(Amount, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// TODO: Converting logic
-
 	baseRate := exchangeRates.Rates[BaseCurrency]
 	convertedRate := exchangeRates.Rates[CovertedCurrency]
-	CovertedAmount := amountFloat * (convertedRate / baseRate)
+	CovertedAmount := Amount * (convertedRate / baseRate)
 
 	fmt.Printf("Converted amout: %.2f %s\n", CovertedAmount, CovertedCurrency)
 
@@ -98,9 +87,18 @@ func main() {
 					huh.NewOption("£ GBP", "GBP"),
 				).
 				Value(&CovertedCurrency),
+
 			huh.NewInput().
-				Title("How much would you want to").
-				Value(&Amount),
+				Title("Enter the amount").
+				Value(&AmountStr). // Store in amountStr instead of amount
+				Validate(func(str string) error {
+					val, err := strconv.ParseFloat(str, 64)
+					if err != nil || val <= 0 {
+						return errors.New("please enter a valid amount")
+					}
+					Amount = val // Assign to amount if parsing is successful
+					return nil
+				}),
 
 			huh.NewConfirm().
 				Title("Would you like to confirm?").
