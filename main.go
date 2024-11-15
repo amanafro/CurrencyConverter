c972b925fd062ca93a0c59f182abd5560b8095e8
amanafro <diazermando193@gmail.com>
2024-11-15 22:38:43 +0100

    the fetching works, the ui needs some work

--- main.go
+++ main.go
@@ -11,61 +11,82 @@ import (
 	"strconv"
 
 	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type ExchangeRates struct {
+	Base  string             `json:"base"`
 	Rates map[string]float64 `json:"rates"`
 }
 
-var (
+type CurrencyConverter struct {
 	BaseCurrency     string
 	CovertedCurrency string
 	Amount           float64
 	AmountStr        string
 	CovertedAmount   float64
 	confirm          bool
-)
+	rates            ExchangeRates
+}
 
-func main() {
+func (cc *CurrencyConverter) convertCurrency() error {
+	baseRate, ok := cc.rates.Rates[cc.BaseCurrency]
+	if !ok {
+		return fmt.Errorf("base currency %s not found", cc.BaseCurrency)
+	}
+	convertedRate, ok := cc.rates.Rates[cc.CovertedCurrency]
+	if !ok {
+		return fmt.Errorf("base currency %s not found", cc.CovertedCurrency)
+	}
+	cc.CovertedAmount = cc.Amount * (convertedRate / baseRate)
+	return nil
+}
 
-	// godotenv.Load(".env")
+func fetchExchangeRate() (ExchangeRates, error) {
 	appID, exists := os.LookupEnv("API_KEY")
 	if !exists {
-		log.Fatalln("API key not found in environment variables. Please set the API_KEY environment variable.")
+		return ExchangeRates{}, errors.New("API key not found in .env")
 	}
 
-	fmt.Println("API Key:", appID)
 	url := fmt.Sprintf("https://openexchangerates.org/api/latest.json?app_id=%s", appID)
 
 	// Make the HTTP request
 	response, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		return ExchangeRates{}, err
 	}
 	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return ExchangeRates{}, fmt.Errorf("API request failed with status: %d", response.StatusCode)
+	}
 
 	// Read the response body
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		return ExchangeRates{}, err
 	}
 
-	// Decode of the json
-
 	var exchangeRates ExchangeRates
-	err = json.Unmarshal(body, &exchangeRates)
+
+	if err := json.Unmarshal(body, &exchangeRates); err != nil {
+		return ExchangeRates{}, err
+	}
+
+	return exchangeRates, nil
+}
+
+func main() {
+
+	converter := &CurrencyConverter{}
+	rates, err := fetchExchangeRate()
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	baseRate := exchangeRates.Rates[BaseCurrency]
-	convertedRate := exchangeRates.Rates[CovertedCurrency]
-	CovertedAmount := Amount * (convertedRate / baseRate)
+	converter.rates = rates
 
-	fmt.Printf("Converted amout: %.2f %s\n", CovertedAmount, CovertedCurrency)
-
-	// TODO: add component to display the converted amount and currency
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewSelect[string]().
@@ -76,7 +97,7 @@ func main() {
 					huh.NewOption("$ USD", "USD"),
 					huh.NewOption("£ GBP", "GBP"),
 				).
-				Value(&BaseCurrency),
+				Value(&converter.BaseCurrency),
 
 			huh.NewSelect[string]().
 				Title("What do you want it to convert into?").
@@ -86,23 +107,23 @@ func main() {
 					huh.NewOption("$ USD", "USD"),
 					huh.NewOption("£ GBP", "GBP"),
 				).
-				Value(&CovertedCurrency),
+				Value(&converter.CovertedCurrency),
 
 			huh.NewInput().
 				Title("Enter the amount").
-				Value(&AmountStr). // Store in amountStr instead of amount
+				Value(&converter.AmountStr). // Store in amountStr instead of amount
 				Validate(func(str string) error {
 					val, err := strconv.ParseFloat(str, 64)
 					if err != nil || val <= 0 {
 						return errors.New("please enter a valid amount")
 					}
-					Amount = val // Assign to amount if parsing is successful
+					converter.Amount = val // Assign to amount if parsing is successful
 					return nil
 				}),
 
 			huh.NewConfirm().
 				Title("Would you like to confirm?").
-				Value(&confirm),
+				Value(&converter.confirm),
 		),
 	)
 
@@ -110,5 +131,22 @@ func main() {
 	err = form.Run()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	// FIX: The display for the converted amount looks out of place
+
+	if converter.confirm {
+		if err := converter.convertCurrency(); err != nil {
+			log.Fatal(err)
+		}
+		var style = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#FAFAFA")).
+			Background(lipgloss.Color("#7D56F4")).
+			PaddingTop(2).
+			PaddingLeft(4).
+			Width(30)
+		s := fmt.Sprintf("Converted amount:%s %0.2f", converter.CovertedCurrency, converter.CovertedAmount)
+		fmt.Println(style.Render(s))
 	}
 }
